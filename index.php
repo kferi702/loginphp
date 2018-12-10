e1bdc346395d0c1bffc74670244fae9a7262a44a
Feri <Feri@DESKTOP-E9BHQOH>
2018-12-10 14:39:48 +0100

    CSS hozzáadva és egy kezdetleges form létrehozva

--- index.php
+++ index.php
@@ -8,10 +8,31 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>FORendelő Bejelentkezés</title>
+          <link rel="stylesheet" href="styles.css">
     </head>
     <body>
+        <form class="container">
+            <img src="FOR_img.bmp" alt="FORendelo">
+            <p>
+                Üdvözöljük rendelőnk honlapján, a belépés után lehetősége nyílik megtekinte kórtörténetét, az előre egyeztetett időpontjait, illetve időpontot kérhet fogorvosától
+            </p>
+        <p id="bejel">Bejelentekzés</p>
+        
+            Felhasznalonév:
+            <input type="text" id="uname "name="uname" placeholder="felhasználonév" required>
+            <br>
+            Jelszó:
+            <input type="password" id="pword" name="pword" placeholder="jelszó" required>
+            <br>
+            <button type="submit" id="submit" name="Belépés">Belépés</button>
+        </form>
         <?php
         // put your code here
         ?>
     </body>
+    <footer>
+        <?php
+                echo "Ma ".date("Y.m.d")." van.";
+        ?>
+    </footer>
 </html>
